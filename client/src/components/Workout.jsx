e0f9a874e88a71982a8bafcf59991e6421b7fdc4
rachel-howell <rhowell96@gmail.com>
2023-03-31 21:45:30 -0700

    Initialize repo

---
+++ client/src/components/Workout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Workout = ({exercises}) => {
+  return (
+    <div>
+        <h1>Here is your workout:</h1>
+        {
+            //map through exercises
+        }
+    </div>
+  )
+}
+
+export default Workout
---
+++ client/src/components/WorkoutCalc.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const WorkoutCalc = () => {
+
+    let allExercises = [
+        { name:"Barbell Squat", type:"barbell" },
+        { name:"Barbell Glute Bridge" },
+    ]
+
+  return (
+    <div>WorkoutCalc</div>
+  )
+}
+
+export default WorkoutCalc
+
+
+// Master list contains all workouts
+// Workout for Hypertrophy or Strength or Endurance determines sets/reps
